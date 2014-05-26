f2e0eb6329af641a23a2200a91b0e0efeac27b47
Torsten Amshove <torsten@amshove.net>
2014-05-26 16:25:19 +0200

    git changed message in new version

--- Tools/publish_new_version.sh
+++ Tools/publish_new_version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Chec for uncommitted changes
-git status | grep "nothing to commit (working directory clean)" > /dev/null 2>&1
+git status | grep "nothing to commit, working directory clean" > /dev/null 2>&1
 if [ $? -ne 0 ]; then
   echo "There are uncommited changes - commit first!"
   exit 1
