5d1920e3f54b8ddcc03425418d83878518107935
lukeIam <mail2skywalker@gmail.com>
2015-03-23 12:52:17 +0100

    - Quickfix for the chrome storage quota problem (use local chrome storage now) #140 (Backup your settings!)

--- Chrome/chromeStorage.js
+++ Chrome/chromeStorage.js
@@ -1,13 +1,13 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	if(typeof(request["getGclhConfig"]) !== "undefined"){
-		chrome.storage.sync.get("GclhConfig", sendResponse);
+		chrome.storage.local.get("GclhConfig", sendResponse);
 		return true;
 	}
 	
     if(typeof(request["setGclhConfig"]) !== "undefined"){
 		var data = {};
 		data["GclhConfig"] = request["setGclhConfig"];
-		chrome.storage.sync.set(data, sendResponse);
+		chrome.storage.local.set(data, sendResponse);
 		return true;
 	}
 	
